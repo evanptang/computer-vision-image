5f1ed38592314ce9955de1fc671d93e210d2b422
Evan Tang <evantang2019@u.northwestern.edu>
2019-07-04 23:39:22 -0700

    Changed for compatibility with web server

--- src/check_functions.py
+++ src/check_functions.py
@@ -10,29 +10,24 @@ def check_slopeintercept ( title, filename):
 	success=True
 	image=read_file.readBmp(filename)
 	line = detection.detect_slope_intercept(image, 640, 480)
-	print(title + " slope intercept results:")
-	print("m: " + str(line.m))
-	print("b: " + str(line.b))
-	print("y = " + str(line.m) +"x + " + str(line.b))
-	print
-	return success
+	#print(title + " slope intercept results:")
+	#print("m: " + str(line.m))
+	#print("b: " + str(line.b))
+	#print("y = " + str(line.m) +"x + " + str(line.b))
+	return [line.m, line.b]
 
 def check_normal (title, filename):
 	success=True
 	image=read_file.readBmp(filename)
 	line = detection.detect_normal(image, 640, 480)
-	print(title + " normal intercept results:")
-	print("theta: " + str(line.theta))
-	print("r: " + str(line.r))
-	print("x*cos(" +str(line.theta) +") + y*sin(" + str(line.theta) + ") = " + str(line.r))
-	print
-	return success
+	#print(title + " normal intercept results:")
+	#print("theta: " + str(line.theta))
+	#print("r: " + str(line.r))
+	#print("x*cos(" +str(line.theta) +") + y*sin(" + str(line.theta) + ") = " + str(line.r))
+	return [line.theta, line.r]
 
 def check_circles ( title, filename):
 	success=True
 	image=read_file.readBmp(filename)
 	circles = detection.detect_circles(image,30,640,480)
-	print(title + " normal intercept results:")
-	print("detected: " + str(circles) + " circles")
-	print
-	return success
+	return circles
